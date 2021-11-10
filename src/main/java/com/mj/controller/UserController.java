cdbea2306932f03f3eda71ee9183dff10fb2c179
avril-coder <avril_for_me@yahoo.co.jp>
2021-11-10 16:15:16 +0900

    added by lding

---
+++ src/main/java/com/mj/controller/UserController.java
@@ -0,0 +1,48 @@
+package com.mj.controller;
+
+import com.mj.po.User;
+import com.mj.result.DataJsonVo;
+import com.mj.result.JsonVo;
+import com.mj.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@Api(tags = "用户")
+public class UserController {
+    @Autowired
+    private UserService service;
+
+    @GetMapping("/list")
+    @ApiOperation("查询所有")
+    public DataJsonVo<List<User>> list() {
+        return new DataJsonVo<>(service.list());
+    }
+
+    @PostMapping("/save")
+    @ApiOperation("添加或更新")
+    public JsonVo save(User user) {
+        JsonVo jsonVo = new JsonVo();
+        jsonVo.setMsg(service.saveOrUpdate(user) ? "保存成功" : "保存失败");
+        return jsonVo;
+    }
+
+    @PostMapping("/json")
+    @ApiOperation("测试")
+    public DataJsonVo<User> json(@RequestBody User user) {
+        return new DataJsonVo<>(user);
+    }
+
+    @PostMapping("/remove")
+    @ApiOperation("删除")
+    public JsonVo remove(Long id) {
+        JsonVo jsonVo = new JsonVo();
+        jsonVo.setMsg(service.removeById(id) ? "删除成功" : "删除失败");
+        return jsonVo;
+    }
+}
---
+++ src/main/java/com/mj/service/UserService.java
@@ -0,0 +1,8 @@
+package com.mj.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.mj.po.User;
+
+public interface UserService extends IService<User> {
+
+}
